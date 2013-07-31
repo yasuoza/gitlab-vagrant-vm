7dfebcbe076507ec3d1e4a0caae1fdaccbb5ee63
Jacob Vosmaer <contact@jacobvosmaer.nl>
2013-07-31 14:01:57 +0200

    Remove unused qt dependencies

--- site-cookbooks/gitlab/attributes/vagrant.rb
+++ site-cookbooks/gitlab/attributes/vagrant.rb
@@ -14,7 +14,7 @@ default['gitlab']['gitlab_branch'] = "master"
 default['gitlab']['packages'] = %w{
   vim curl wget checkinstall libxslt-dev
   libcurl4-openssl-dev libssl-dev libmysql++-dev
-  libicu-dev libc6-dev libyaml-dev python python-dev libqt4-dev libqtwebkit-dev
+  libicu-dev libc6-dev libyaml-dev python python-dev
   xvfb xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic
 }
 
